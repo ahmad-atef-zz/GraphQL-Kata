c5812407bb8be7d8f09ba0cd95fdf0a7e988b377
Ahmad Atef <ahmad.atef.ali.ahmad@gmail.com>
2021-06-06 17:36:52 +0200

    Adding ApolloCodegen
    Signed-off-by: Ahmad Atef <ahmad.atef.ali.ahmad@gmail.com>

---
+++ GraphQL-Kata/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GraphQL-Kata
+//
+//  Created by Ahmed Atef Ali Ahmed on 06.06.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
